c1b6b36186d3a530593cf94b485e4f8eeeac326a
hishambokhari <hish.b92@gmail.com>
2020-07-12 00:47:09 +0500

    Storing with JSON

--- playground/1-json.js
+++ playground/1-json.js
@@ -1,11 +1,26 @@
 const fs = require('fs')
-const book = {
-  title: 'Ego is the enemy',
-  author: 'Ryan Holiday'
-}
+// const book = {
+//   title: 'Ego is the enemy',
+//   author: 'Ryan Holiday'
+// }
 
-const bookJSON = JSON.stringify(book) //bookJson is a string not an object, converting to a string via JSONStringify
-console.log(bookJSON)
+// const bookJSON = JSON.stringify(book) //bookJson is a string not an object, converting to a string via JSONStringify
+// fs.writeFileSync('1-json.json', bookJSON)
 
-const parsedData = JSON.parse(bookJSON) // turns string back to object
-console.log(parsedData.author)
+// const dataBuffer = fs.readFileSync('1-json.json')
+// const dataJSON = dataBuffer.toString()
+// const data = JSON.parse(dataJSON)
+// console.log(data.title)
+
+
+
+const dataBuffer = fs.readFileSync('1-json.json')
+const dataJSON = dataBuffer.toString()
+const user = JSON.parse(dataJSON)
+
+
+user.name = 'Hisham'
+user.age = 28
+
+const userJSON = JSON.stringify(user)
+fs.writeFileSync('1-json.json', userJSON)
