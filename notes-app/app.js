016a597d92d5d18483fd82ba693399482711170a
hishambokhari <hish.b92@gmail.com>
2020-07-12 00:23:16 +0500

    yargs parse

--- notes-app/app.js
+++ notes-app/app.js
@@ -10,8 +10,21 @@ yargs.version('1.1.0')
 yargs.command({
   command: 'add',  
   describe: 'Add a new note',
-  handler: function() {
-    console.log('Adding a new note')
+  builder: {
+    title: {
+      describe: 'Note title',
+      demandOption: true,
+      type: 'string'
+    },
+    body: {
+      description: 'This is the body section',
+      demandOption: true,
+      type: 'string'
+    }
+  },
+  handler: function(argv) {
+    console.log('Title: ' + argv.title)
+    console.log('Body: ' + argv.body)
   }
 })
 
@@ -44,8 +57,7 @@ yargs.command({
 
 // add, remove, read and list
 
-
-console.log(yargs.argv)
+yargs.parse()
 
 
 
