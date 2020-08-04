903d9459cc6d3b1b83e32259703897545e76ec1f
hishambokhari <hish.b92@gmail.com>
2020-08-04 13:55:42 +0100

    chalk package

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,4 +1,10 @@
+const chalk = require('chalk')
+const validator = require('validator');
 const getNotes = require('./notes');
 
+
+console.log(chalk.green('This is a success!'))
 const myNotes = getNotes()
 console.log(myNotes)
+
+console.log(validator.isURL('https://mead.io')) // url validator
