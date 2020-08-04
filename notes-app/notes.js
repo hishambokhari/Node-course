3cb257e9384b712c9b1d00c21015c6dc297e0a9d
hishambokhari <hish.b92@gmail.com>
2020-08-04 13:43:58 +0100

    nod emoudles

--- notes-app/notes.js
+++ notes-app/notes.js
@@ -1,36 +1,5 @@
-const fs = require('fs')
-
-const getNotes = function(){
+function getNotes(){
   return "Your notes..."
 }
 
-const addNote = function (title, body) {
-  const notes = loadNotes()
-
-  notes.push({
-    title: title,
-    body: body
-  })
-
-  savedNotes(notes)
-}
-
-const savedNotes = function (notes) {
-  const dataJSON = JSON.stringify(notes)
-  fs.writeFileSync('notes.json', dataJSON)
-}
-
-const loadNotes = function () {
-  try {
-    const dataBuffer = fs.readFileSync('notes.json')
-    const dataJSON = dataBuffer.toString()
-    return JSON.parse(dataJSON)
-  } catch (e) {
-    return []
-  }
-} 
-
-module.exports = {
-  getNotes: getNotes,
-  addNote: addNote
-}
+module.exports = getNotes
